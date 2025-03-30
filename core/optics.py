0c5a1a843df3474ea2beb8df4ed37238632a18ff
Molks <a3673@basisit.de>
2025-03-30 14:12:22 +0200

    menubar apdate OTG

---
+++ core/optics.py
@@ -0,0 +1,14 @@
+def plotting(data, style):
+    pass
+    #possible adjustables 
+#linear functions 
+def beta_calc(lattice, start):
+    pass
+    #return data array not plots for visualization of produkts or familiar
+def disp_calc(lattice, start):
+    pass
+# nonlinear functions
+
+def alpha_calc(lattice, order):
+    pass
+    #Alpha als ganzes und die einzelnen Beiträge
--- ui/main_window.py
+++ ui/main_window.py
@@ -15,12 +15,30 @@ class MainWindow(QMainWindow):
 
     def create_menu(self):
         menu_bar = QMenuBar(self)
+    #Menus
         file_menu = QMenu("&Datei", self)
+        parameter_menu = QMenu("parameters", self)
         linear_menu = QMenu("linear", self)
         nonlinear_menu = QMenu("nonlinear", self)
-        open_action = QAction("load Lattice", self)
+    #Menu-actions
+        open_action = QAction("Load", self)
+        save_action = QAction("Save", self)
+        saveas_action = QAction("Save as...", self)
+        export_action = QAction("Export",self)
+
+        parameter_action = QAction("parameters", self)
+
+        linopt_action = QAction("open linopt", self)
+
+        nonlinopt_action = QAction("open nonlinopt")
+        
+    #Adding everything
         file_menu.addAction(open_action)
+        parameter_menu.addAction(parameter_action)
+        linear_menu.addAction(linopt_action)
+        nonlinear_menu.addAction(nonlinopt_action)
         menu_bar.addMenu(file_menu)
+        menu_bar.addMenu(parameter_menu)
         menu_bar.addMenu(linear_menu)
         menu_bar.addMenu(nonlinear_menu)
         self.setMenuBar(menu_bar)
